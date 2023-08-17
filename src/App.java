4fcef1d33559eb56b1f8752bc30c091f4943c49d
Andrea <a01642531@tec.mx>
2023-08-16 19:06:30 -0600

    Mis canciones favoritas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("El rey azul -Emmanuel");
+        System.out.println("Feel Good Inc -");
+        System.out.println("I was made for loving you - KISS");
+        System.out.println("The way you make me feel - Michael Jackson");
+        System.out.println("Thunderstruck - AC/DC");
+        System.out.println("Once in a Moon - Sahara Kang");
     }
 }
